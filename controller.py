9ded91e9e38dba85ba14cb7c84e197b67f512161
diominvd <diomin.vd13@icloud.com>
2023-11-20 22:24:24 +0300

    Complete reset timer, change timer mode.

--- controller.py
+++ controller.py
@@ -22,12 +22,25 @@ class Controller:
 		# Configure view:time_label
 		self.view.time_label.configure(text=time)
 
+	def timer_option_menu_event(self, choice: str) -> str:
+		match choice:
+			case "Work":
+				self.model.mode = "work"
+			case "Break":
+				self.model.mode = "break"
+		# Update timer
+		self.reset_timer()
+		return self.model.mode
+
 	def start_timer_button_event(self) -> None:
+		# Configure timer buttons:state
+		self.view.start_timer_button.configure(state="disabled")
+		self.view.pause_timer_butoon.configure(state="normal")
+		self.view.reset_timer_button.configure(state="normal")
 		self.model.change_model_status(key="start")
 		self.update_timer()
 
 	def update_timer(self) -> None:
-		# self.model.status = True
 		if self.model.status:
 			# Check end of timer
 			if (self.model.hours, self.model.minutes, self.model.seconds) == (0, 0, 0):
@@ -47,3 +60,24 @@ class Controller:
 		text = "Pause" if self.model.status == True else "Resume"
 		# Configure pause_button:text
 		self.view.pause_timer_butoon.configure(text=text)
+
+	def reset_timer_button_event(self) -> None:
+		self.model.change_model_status(key="reset")
+		self.reset_timer()
+
+	def reset_timer(self) -> None:
+		# Configure timer buttons:state
+		self.view.start_timer_button.configure(state="normal")
+		self.view.pause_timer_butoon.configure(state="disabled")
+		self.view.reset_timer_button.configure(state="disabled")
+		# Configure pause_button:text
+		self.view.pause_timer_butoon.configure(text="Pause")
+		# Stop update_timer
+		try:
+			self.view.after_cancel(self.model.id)
+		except ValueError:
+			pass
+		finally:
+			self.model.id = None
+			self.load_time()
+			self.update_time_label()
--- view.py
+++ view.py
@@ -34,12 +34,12 @@ class View(ctk.CTk):
 		# Label:
 		self.time_label.configure(font=("Calibri", 30, "bold"), text="TIME:TEXT")
 		# OptionMenu:
-		self.timer_option_menu.configure(font=("Calibri", 14), values=["work".title(), "break".title()], command=None)
+		self.timer_option_menu.configure(font=("Calibri", 14), values=["work".title(), "break".title()], command=self.timer_option_menu_choice_handler)
 		self.timer_option_menu.set("work".title())
 		# Buttons:
 		self.start_timer_button.configure(state="normal", font=("Calibri", 14), text="Start", command=self.start_timer_button_click_handler)
 		self.pause_timer_butoon.configure(state="disabled", font=("Calibri", 14), text="Pause", command=self.pause_timer_button_click_handler)
-		self.reset_timer_button.configure(state="disabled", font=("Calibri", 14), text="Reset", command=None)
+		self.reset_timer_button.configure(state="disabled", font=("Calibri", 14), text="Reset", command=self.reset_timer_button_click_handler)
 
 	def palace_tab1_widgets(self) -> None:
 		self.tabview.pack(expand=True, fill="both")
@@ -49,15 +49,19 @@ class View(ctk.CTk):
 		self.pause_timer_butoon.pack(padx=0, pady=5)
 		self.reset_timer_button.pack(padx=0, pady=5)
 
+	def timer_option_menu_choice_handler(self, choice) -> None:
+		#  Call choice event
+		self.controller.timer_option_menu_event(choice)
+
 	def start_timer_button_click_handler(self) -> None:
-		# Configure timer buttons:state
-		self.start_timer_button.configure(state="disabled")
-		self.pause_timer_butoon.configure(state="normal")
-		self.reset_timer_button.configure(state="normal")
 		# Call start event
 		self.controller.start_timer_button_event()
 
 	def pause_timer_button_click_handler(self) -> None:
 		# Call pause event
 		self.controller.pause_timer_button_event()
+
+	def reset_timer_button_click_handler(self) -> None:
+		# Call reset event
+		self.controller.reset_timer_button_event()
 
