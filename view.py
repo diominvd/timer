e985ec446c6d8142238f7d9c0be090bdcd1576f2
diominvd <diomin.vd13@icloud.com>
2023-11-26 00:10:51 +0300

    UI edits

--- view.py
+++ view.py
@@ -1,12 +1,21 @@
 import customtkinter as ctk
 
 
+class UI():
+	def __init__(self):
+		self.PADX: int = 5
+		self.PADY: int = 5
+		self.CLR_ACCENT_1: str = "#00a5ff"
+		self.CLR_ACCENT_2: str = "#008fdb"
+		self.CLR_ACCENT_3: str = "#0077b8"
+		self.CLR_BUTTON_TEXT_NORMAL: str = "#fcfcfc"
+		self.CLR_BUTTON_TEXT_DISABLED: str = "#dadada"
+
+
 class View(ctk.CTk):
 	def __init__(self, controller):
 		super().__init__()
-		# CONST
-		self.PADX = 5
-		self.PADY = 5
+		self.ui = UI()
 		# Connect controller
 		self.controller = controller
 
@@ -18,6 +27,9 @@ class View(ctk.CTk):
 		self.tabview = ctk.CTkTabview(self)
 		self.tabview.add("Timer")
 		self.tabview.add("Settings")
+
+		# Configure TabView
+		self.tabview.configure(segmented_button_selected_color=self.ui.CLR_ACCENT_1, segmented_button_selected_hover_color=self.ui.CLR_ACCENT_2)
 
 		# Widgets -> Tab:"Timer" -> TabView
 		self.time_label = ctk.CTkLabel(self.tabview.tab("Timer"))
@@ -43,45 +55,44 @@ class View(ctk.CTk):
 	def configure_tab1_widgets(self) -> None:
 		# TabView:
 		self.tabview._segmented_button._buttons_dict["Timer"].configure(font=("Calibri", 14)) # TabView:Tab:Timer
-		self.tabview.tab("Settings").grid_columnconfigure(0, weight=1)
 		# Label:
 		self.time_label.configure(font=("Calibri", 30, "bold"), text=None)
 		# OptionMenu:
-		self.timer_option_menu.configure(font=("Calibri", 14), values=["work".title(), "break".title()], command=self.timer_option_menu_choice_handler)
+		self.timer_option_menu.configure(font=("Calibri", 14), fg_color=self.ui.CLR_ACCENT_1, button_color=self.ui.CLR_ACCENT_2, button_hover_color=self.ui.CLR_ACCENT_3, values=["work".title(), "break".title()], command=self.timer_option_menu_choice_handler)
 		self.timer_option_menu.set("work".title())
 		# Buttons:
-		self.start_timer_button.configure(state="normal", font=("Calibri", 14), text="Start", command=self.start_timer_button_click_handler)
-		self.pause_timer_butoon.configure(state="disabled", font=("Calibri", 14), text="Pause", command=self.pause_timer_button_click_handler)
-		self.reset_timer_button.configure(state="disabled", font=("Calibri", 14), text="Reset", command=self.reset_timer_button_click_handler)
+		self.start_timer_button.configure(state="normal", font=("Calibri", 14), text="Start", text_color=self.ui.CLR_BUTTON_TEXT_NORMAL, text_color_disabled=self.ui.CLR_BUTTON_TEXT_DISABLED, fg_color=self.ui.CLR_ACCENT_1, hover_color=self.ui.CLR_ACCENT_3,  command=self.start_timer_button_click_handler)
+		self.pause_timer_butoon.configure(state="disabled", font=("Calibri", 14), text="Pause", text_color=self.ui.CLR_BUTTON_TEXT_NORMAL, text_color_disabled=self.ui.CLR_BUTTON_TEXT_DISABLED, fg_color=self.ui.CLR_ACCENT_1, hover_color=self.ui.CLR_ACCENT_3,  command=self.pause_timer_button_click_handler)
+		self.reset_timer_button.configure(state="disabled", font=("Calibri", 14), text="Reset", text_color=self.ui.CLR_BUTTON_TEXT_NORMAL, text_color_disabled=self.ui.CLR_BUTTON_TEXT_DISABLED, fg_color=self.ui.CLR_ACCENT_1, hover_color=self.ui.CLR_ACCENT_3,  command=self.reset_timer_button_click_handler)
 
 	def configure_tab2_widgets(self) -> None:
 		# TabView:
 		self.tabview._segmented_button._buttons_dict["Settings"].configure(font=("Calibri", 14)) # TabView:Tab:Settings
 		# OptionMenu:
-		self.settings_timer_option_menu.configure(font=("Calibri", 14), values=["work".title(), "break".title()], command=self.timer_option_menu_choice_handler)
+		self.settings_timer_option_menu.configure(font=("Calibri", 14), fg_color=self.ui.CLR_ACCENT_1, button_color=self.ui.CLR_ACCENT_2, button_hover_color=self.ui.CLR_ACCENT_3, values=["work".title(), "break".title()], command=self.settings_timer_option_menu_choice_handler)
 		self.settings_timer_option_menu.set("work".title())
 		# Label:
 		self.settings_time_label.configure(font=("Calibri", 30, "bold"), text=None)
 		self.settings_time_label.bind("<MouseWheel>", None)
-		self.settings_hint_label.configure(font=("Calibri", 13), text_color="#cbcbcb", text="Scroll label to edit")
+		self.settings_hint_label.configure(font=("Calibri", 13), text_color=self.ui.CLR_BUTTON_TEXT_NORMAL, text="Scroll label to edit")
 		# Buttons
-		self.settings_edit_button.configure(state="normal", font=("Calibri", 14), text="Edit", command=self.settings_edit_button_click_handler)
-		self.settings_save_button.configure(state="disabled", font=("Calibri", 14), text="Save", command=self.settings_save_button_click_handler)
-		self.settings_cancel_button.configure(state="normal", font=("Calibri", 14), text="Cancel",command=self.settings_cancel_button_click_handler)
+		self.settings_edit_button.configure(state="normal", font=("Calibri", 14), text="Edit", text_color=self.ui.CLR_BUTTON_TEXT_NORMAL, text_color_disabled=self.ui.CLR_BUTTON_TEXT_DISABLED, fg_color=self.ui.CLR_ACCENT_1, hover_color=self.ui.CLR_ACCENT_3,   command=self.settings_edit_button_click_handler)
+		self.settings_save_button.configure(state="disabled", font=("Calibri", 14), text="Save", text_color=self.ui.CLR_BUTTON_TEXT_NORMAL, text_color_disabled=self.ui.CLR_BUTTON_TEXT_DISABLED, fg_color=self.ui.CLR_ACCENT_1, hover_color=self.ui.CLR_ACCENT_3,   command=self.settings_save_button_click_handler)
+		self.settings_cancel_button.configure(state="normal", font=("Calibri", 14), text="Cancel", text_color=self.ui.CLR_BUTTON_TEXT_NORMAL, text_color_disabled=self.ui.CLR_BUTTON_TEXT_DISABLED, fg_color=self.ui.CLR_ACCENT_1, hover_color=self.ui.CLR_ACCENT_3,  command=self.settings_cancel_button_click_handler)
 
 	def palace_tab1_widgets(self) -> None:
 		self.tabview.pack(expand=True, fill="both")
-		self.time_label.pack(padx=0, pady=self.PADY*3)
-		self.timer_option_menu.pack(padx=0, pady=self.PADY)
-		self.start_timer_button.pack(padx=0, pady=self.PADY)
-		self.pause_timer_butoon.pack(padx=0, pady=self.PADY)
-		self.reset_timer_button.pack(padx=0, pady=self.PADY)
+		self.time_label.pack(padx=0, pady=self.ui.PADY*3)
+		self.timer_option_menu.pack(padx=0, pady=self.ui.PADY)
+		self.start_timer_button.pack(padx=0, pady=self.ui.PADY)
+		self.pause_timer_butoon.pack(padx=0, pady=self.ui.PADY)
+		self.reset_timer_button.pack(padx=0, pady=self.ui.PADY)
 
 	def place_tab2_widgets(self) -> None:
-		self.settings_time_label.pack(padx=0, pady=self.PADY*3)
-		self.settings_timer_option_menu.pack(padx=0, pady=self.PADY)
-		self.settings_edit_button.pack(padx=0, pady=self.PADY)
-		self.settings_save_button.pack(padx=0, pady=self.PADY)
+		self.settings_time_label.pack(padx=0, pady=self.ui.PADY*3)
+		self.settings_timer_option_menu.pack(padx=0, pady=self.ui.PADY)
+		self.settings_edit_button.pack(padx=0, pady=self.ui.PADY)
+		self.settings_save_button.pack(padx=0, pady=self.ui.PADY)
 
 	def timer_option_menu_choice_handler(self, choice) -> None:
 		#  Call choice event
@@ -102,7 +113,7 @@ class View(ctk.CTk):
 	def settings_time_label_scroll_handler(self, event) -> None:
 		self.controller.settings_time_label_scroll_event(event)
 
-	def timer_option_menu_choice_handler(self, choice) -> None:
+	def settings_timer_option_menu_choice_handler(self, choice) -> None:
 		self.controller.settings_timer_option_menu_event()
 
 	def settings_edit_button_click_handler(self) -> None:
