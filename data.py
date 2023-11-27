295b977bc6f170604867fbaa5d0b65ef770be3f3
diominvd <diomin.vd13@icloud.com>
2023-11-27 22:04:33 +0300

    Structure edits

--- data.py
+++ data.py
@@ -8,23 +8,23 @@ class Data:
 		self.create_data_file()
 
 	def create_data_file(self) -> None:
-		if os.path.isfile("data.json"):
+		if os.path.isfile("data/data.json"):
 			pass
 		else:
-			file = open("data.json", "w")
+			file = open("data/data.json", "w")
 			data: dict = {"timer": {"work": {"hours": 0, "minutes": 45, "seconds": 0}, "break": {"hours": 0, "minutes": 5, "seconds": 0}}, "theme": {"current_theme": "green"}}
 			json.dump(data, file, indent=4)
 
 	def create_themes_file(self) -> None:
-		if os.path("themes.json"):
+		if os.path("data/themes.json"):
 			pass
 		else:
-			file = open("themes.json", "w")
+			file = open("data/themes.json", "w")
 			data: dict = {"themes": {"lime": ["#daff73", "#c2ff1c", "#ace219", "#99c916", "#ffffff", "#383838", "#4e4e4e"], "cyan": ["#b0fffc", "#00fff6", "#00d6ce", "#00b3ad", "#ffffff", "#383838", "#4e4e4e"], "ocean": ["#6eabff", "#006aff", "#005ad7", "#004cb6", "#ffffff", "#383838", "#4e4e4e"], "red": ["#ff6c9d", "#ff0055", "#d10046", "#a80038", "#ffffff", "#383838", "#4e4e4e"], "orange": ["#ffbb80", "#ff7700", "#da6600", "#ba5700", "#ffffff", "#383838", "#4e4e4e"]}, "current_theme": "cyan"}
 			json.dump(data, file, indent=4)
 
 	def load_themes(self) -> dict:
-		with open("themes.json", "r") as file:
+		with open("data/themes.json", "r") as file:
 			themes: dict = json.load(file)["themes"]
 			file.close()
 		# Create themes
@@ -38,17 +38,17 @@ class Data:
 		return themes
 
 	def load_current_theme(self) -> str:
-		with open("themes.json", "r") as file:
+		with open("data/themes.json", "r") as file:
 			theme: str = json.load(file)["current_theme"]
 			file.close()
 			return theme
 
 	def fix_theme(self, choice) -> None:
-		with open("themes.json", "r") as file:
+		with open("data/themes.json", "r") as file:
 			data: dict = json.load(file)
 			file.close()
 		data["current_theme"] = choice.lower()
-		with open("themes.json", "w") as file:
+		with open("data/themes.json", "w") as file:
 			json.dump(data, file, indent=4)
 			file.close()
 
@@ -61,7 +61,7 @@ class Data:
 		return {"hours": hours, "minutes": minutes, "seconds": seconds}
 
 	def load_data(self, key=None) -> dict:
-		with open("data.json", "r") as file:
+		with open("data/data.json", "r") as file:
 			data: dict = json.load(file) # Whole data
 			file.close()
 		match key:
@@ -79,7 +79,7 @@ class Data:
 		return data["hours"], data["minutes"], data["seconds"]
 
 	def write_data(self, data: dict) -> None:
-		with open("data.json", "w") as file:
+		with open("data/data.json", "w") as file:
 			json.dump(data, file, indent=4)
 			file.close()
 			
